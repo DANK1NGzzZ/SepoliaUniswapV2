6db41e9af3f2c393d7b04a2888d9bfc77d540094
ywh <2097763336@qq.com>
2024-07-10 03:17:36 +0800

    first commit

---
+++ src/constants/lists.ts
@@ -0,0 +1,6 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'https://umaproject.org/uma.tokenlist.json'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL
+]
---
+++ src/constants/multicall/index.ts
@@ -0,0 +1,13 @@
+import { ChainId } from 'sdk-sepolia-ywh'
+import MULTICALL_ABI from './abi.json'
+
+const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441',
+  [ChainId.ROPSTEN]: '0x53C43764255c17BD724F74c4eF150724AC50a3ed',
+  [ChainId.KOVAN]: '0x2cc8688C5f75E365aaEEb4ea8D6a480405A48D2A',
+  [ChainId.RINKEBY]: '0x42Ad527de7d4e9d9d011aC45B31D8551f8Fe9821',
+  [ChainId.GÖRLI]: '0x77dCa2C955b15e9dE4dbBCf1246B4B85b651e50e',
+   [ChainId.Sepolia]: '0x3d945C27Df6D997F3F553a9672C92ce3E2aB6213'
+}
+
+export { MULTICALL_ABI, MULTICALL_NETWORKS }
