9444d5dae60f14157b952ce5707bd824606cb8eb
ywh <2097763336@qq.com>
2024-07-10 21:50:42 +0800

    Update project with latest changes

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://umaproject.org/uma.tokenlist.json'
+export const DEFAULT_TOKEN_LIST_URL = 'https://ipfs.filebase.io/ipfs/QmQiRTKoc6uMbRefDfnuJ4v6TtPgsHdzpB8NBAKUs8P35v'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL
